5c12d9428f550d9600f6f288ad77e8d16794d67f
Hurricane36 <43070976+Hurricane36@users.noreply.github.com>
2018-10-22 08:07:50 -0600

    imported test files

---
+++ src/chat/controller/ChatController.java
@@ -0,0 +1,26 @@
+package chat.controller;
+import javax.swing.JOptionPane;
+
+import chat.model.ChatBot;
+
+public class ChatController
+{
+	private ChatBot SimpleBot;
+	
+	public ChatController()
+	{
+		SimpleBot = new ChatBot();
+	}
+	public void start()
+	{
+		String userText = "";
+		while (!userText.equalsIgnoreCase("Quit"))
+		{
+			
+		}
+	}
+	public String interactWithChatBot()
+	{
+		return null;
+	}
+}
---
+++ src/chat/model/ChatBot.java
@@ -0,0 +1,50 @@
+package chat.model;
+import java.util.ArrayList;
+public class ChatBot
+{
+	ArrayList responseList = new ArrayList();
+	ArrayList spookyList =  new ArrayList();
+	private String joke;
+	private String currentUser;
+	private String content;
+
+	public ChatBot()
+	{
+		this.joke = "Why did the monkey cross the road? It was stapled to the chicken";
+		this.currentUser = new String("default user - boring!!!");
+		this.content = new String("empty of all content but not null");
+		
+		this.responseList = new ArrayList<String>();
+		this.spookyList = new ArrayList<String>();
+		buildTheLists();
+		
+	}
+	
+	private void buildTheLists()
+	{
+		responseList.add("Hello! How are you?");
+		responseList.add("Goodbyr - no more talking!");
+		responseList.add("Whats your name?");
+		responseList.add("Whats your mothers maiden name?");
+		responseList.add("What was the name of your first pet?");
+		
+		spookyList.add("Halloween more spooktober");
+		spookyList.add("Your identity was stolen!!!");
+		spookyList.add("Your car was stolen");
+		spookyList.add("BOOOOOO!");
+		spookyList.add("Turn around.......");
+		spookyList.add("RUN!!!!!!");
+		
+	}
+	
+	public String processText(String userText)
+	{
+		String output = "";
+		
+		output += "You said:" + userText;
+		
+		return output;
+		
+	}
+}
+
