59a574760a2831f1936cf1e0a56b71ceee035bcd
Hurricane36 <43070976+Hurricane36@users.noreply.github.com>
2018-12-05 08:12:45 -0700

    Gui

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,18 +1,97 @@
 package chat.view;
-
 import javax.swing.JPanel;
-
 import chat.controller.ChatController;
 
+import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.*;
 
 public class ChatPanel extends JPanel
 {
-
-	public ChatPanel(ChatController appController)
+	private ChatController app;
+	private JTextField chatField;
+	private JTextArea chatArea;
+	private SpringLayout appLayout;
+	private JButton saveButton;
+	private JButton loadButton;
+	private JButton chatButton;
+	private JButton checkerButton;
+	private JScrollPane chatPane;
+	
+	
+	
+	public ChatPanel(ChatController app)
 	{
 		super();
+		
+		this.app = app;
+		appLayout = new SpringLayout();
+		
+		saveButton = new JButton("Save");
+		loadButton = new JButton("Load");
+		chatButton = new JButton("Chat");
+		checkerButton = new JButton("Check Text");
+		
+		chatField = new JTextField("Talk to the bot here", 50);
+		chatArea = new JTextArea("Chat Area", 20, 50);
+		
+		
+		chatPane = new JScrollPane();
+		
+		setupScrollPane();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+			
 	}
+	private void setupScrollPane()
+	{
+		chatArea.setEditable(false);
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+		
+		chatPane.setViewportView(chatArea);
+		chatPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		chatPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.setPreferredSize(new Dimension(800,600));
+		this.setBackground(Color.RED);
+		this.add(chatPane);
+		this.add(chatButton);
+		this.add(saveButton);
+		this.add(loadButton);
+		this.add(checkerButton);
+		this.add(chatField);	
+	}
+	
+	private void setupLayout()
+	{
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
